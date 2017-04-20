6538d1c890383f6125581c2c92ba96be00c59100
Robert Von Zimmerman <vonzimr@gmail.com>
2017-04-20 16:17:10 -0700

    add socket.io-client, which currently listens on a hardcoded ip address. Should fix that. Also added an animation component and started playing with registering my own components in a components.js file

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,7 @@
 require('aframe');
 require('aframe-animation-component');
-require('./rotate.js');
-require('./scale.js');
+require('./components.js');
+///TODO: Break this out into a file or config or something? Hardcoded
+//addresses are kind of silly.
+var socket = require('socket.io-client')("http://192.168.0.118:3002");
+
