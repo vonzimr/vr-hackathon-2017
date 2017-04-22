d1b0d9117ea4a89d0a99fa2072c9e3658e4197c6
rdvonz <rdvonz@gmail.com>
2017-04-22 10:45:25 -0700

    add follow component from tutorial

--- src/js/components.js
+++ src/js/components.js
@@ -1,34 +1,32 @@
-AFRAME.registerComponent('log', {
-    schema: {
-        event: {type: 'string', default: ''},
-        message: {type: 'string', default: 'Hello, World!'}
-    },
-    init: function () {
-        var self = this;
-        this.eventHandlerFn = function () { console.log(self.data.message); };
-    },
-    update: function (oldData) {
-        var data = this.data;
-        var el = this.el;
-        // `event` updated. Remove the previous event listener if it exists.
-        if (oldData.event && data.event !== oldData.event) {
-            el.removeEventListener(oldData.event, this.eventHandlerFn);
-        }
-        if (data.event) {
-            el.addEventListener(data.event, this.eventHandlerFn);
-        } else {
-            console.log(data.message);
-        }
-    },
-
-    /*
-     * Handle Component Removal
-     */
-    remove:function() {
-        var data = this.data;
-        var el = this.el;
-        if(data.event){
-            el.removeEventListener(data.event, this.eventHandlerFn);
-        }
-    }
+AFRAME.registerComponent('follow', {
+  schema: {
+    target: {type: 'selector'},
+    speed: {type: 'number'}
+  },
+  init: function () {
+    this.directionVec3 = new THREE.Vector3();
+  },
+  tick: function (time, timeDelta) {
+    var directionVec3 = this.directionVec3;
+    // Grab position vectors (THREE.Vector3) from the entities' three.js objects.
+    var targetPosition = this.data.target.object3D.position;
+    var currentPosition = this.el.object3D.position;
+    // Subtract the vectors to get the direction the entity should head in.
+    directionVec3.copy(targetPosition).sub(currentPosition);
+    // Calculate the distance.
+    var distance = directionVec3.length();
+    // Don't go any closer if a close proximity has been reached.
+    if (distance < 1) { return; }
+    // Scale the direction vector's magnitude down to match the speed.
+    var factor = this.data.speed / distance;
+    ['x', 'y', 'z'].forEach(function (axis) {
+      directionVec3[axis] *= factor * (timeDelta / 1000);
+    });
+    // Translate the entity in the direction towards the target.
+    this.el.setAttribute('position', {
+      x: currentPosition.x + directionVec3.x,
+      y: currentPosition.y + directionVec3.y,
+      z: currentPosition.z + directionVec3.z
+    });
+  }
 });
--- src/js/main.js
+++ src/js/main.js
@@ -3,5 +3,5 @@ require('aframe-animation-component');
 require('./components.js');
 ///TODO: Break this out into a file or config or something? Hardcoded
 //addresses are kind of silly.
-var socket = require('socket.io-client')("http://192.168.0.118:3002");
+var socket = require('socket.io-client')("http://192.168.42.86:3002");
 
