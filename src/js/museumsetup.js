86168629dfb57cf0c707ca3bb5acd06f369e6507
Jacob R <tapdancingmetroid@gmail.com>
2017-04-22 15:02:37 -0700

    museumsetup now places cylinders correctly, based on img instead of whatever asset tag though

--- src/js/museumsetup.js
+++ src/js/museumsetup.js
@@ -3,24 +3,19 @@ AFRAME.registerComponent('museumsetup', {
   init: function () {
     var cylinder_z = -8
     scene = document.querySelector('a-scene');
-    /*
+
     function addArt() {
-      log.out("beep");
       cylinderEntity = document.createElement('a-cylinder');
       cylinderEntity.setAttribute('position', {x: 1, y: 0, z: cylinder_z});
+      cylinderEntity.setAttribute('color', 'red');
+      cylinderEntity.setAttribute('height', 2.5);
+      cylinderEntity.setAttribute('radius', 0.25);
       scene.appendChild(cylinderEntity);
       cylinder_z -= 3;
     }
 
     document.querySelector('a-assets');
     a = document.querySelectorAll('img');
-    a.forEach(addArt(x));
-    */
-    cylinderEntity = document.createElement('a-cylinder');
-    cylinderEntity.setAttribute('position', {x: 1, y: 0, z: cylinder_z});
-    cylinderEntity.setAttribute('color', 'red');
-    cylinderEntity.setAttribute('height', 2.5);
-    cylinderEntity.setAttribute('radius', 0.25);
-    scene.appendChild(cylinderEntity);
+    a.forEach(function(x) {addArt();});
   }
 });
