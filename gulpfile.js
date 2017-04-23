244dac4d2e4838f0caec54d63eb8fed572e4ece3
rdvonz <rdvonz@gmail.com>
2017-04-23 09:20:01 -0700

    remove unnecessary options in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -9,26 +9,6 @@ var gls = require('gulp-live-server');
 var inputPages     = 'src/pages/**/*.+(html|nunjucks|njk)';
 var inputTemplates = 'src/templates/**/*.+(html|nunjucks|njk)';
 
-gulp.task('sass', function(){
-  return gulp.src('src/scss/**/*.scss')
-  .pipe(sass())
-  .pipe(gulp.dest('dist/css'))
-  .pipe(browserSync.reload({
-    stream: true
-  }));
-});
-
-
-
-gulp.task('nunjucks', function(){
-  //gets .html, .njk, .nunjuks files in pages
-  return gulp.src(inputPages)
-  .pipe(nunjucksRender({
-    path: ['src/templates']
-  }))
-  .pipe(gulp.dest('dist'));
-});
-
 
 gulp.task('browserify', function() {
     return browserify('./src/js/main.js')
@@ -48,10 +28,7 @@ gulp.task('watch', function (){
   var server = gls.new(__dirname + '/app.js');
   serverNotify = function(file){server.notify(file);};
   server.start();
-  gulp.watch('src/scss/**/*.scss', ['sass']);
-  gulp.watch('dist/*.html', serverNotify);
   gulp.watch('dist/js/*.js', serverNotify);
   gulp.watch('src/js/**/*.js', ['browserify']);
-  gulp.watch([inputPages, inputTemplates], ['nunjucks']);
   //other watchers
 });
