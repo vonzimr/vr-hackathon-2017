c1497821d35485de42d244172db071701ab51de7
Crifen <connor.rifenbery@gmail.com>
2017-04-22 22:58:49 -0700

    roooot dir

--- AssetScrape.py
+++ AssetScrape.py
@@ -12,7 +12,7 @@ import sys
 import requests
 from lxml import html
 import subprocess
-root_dir = "."
+root_dir = os.path.dirname(os.path.abspath(__file__))
 def BlendtoObj(Blendfile):
     if Blendfile.endswith(".blend"):
         Blendout, Blendext = os.path.splitext(Blendfile)
